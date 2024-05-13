7e38999ba4b7cf68427120f8c3a28e49fb7d167f
mohamedhanif045 <hanifmohamed045@gmail.com>
2024-05-13 18:36:09 +0300

    assignment of controlflow lab

--- index.js
+++ index.js
@@ -9,3 +9,48 @@ function ternaryCheckCity(){
 function switchOnCharmFromTip(){
   // Write your code here!
 }
+function scuberGreetingForFeet(distance) {
+  if (distance <= 400) {
+    return 'This one is on me!';
+  } else if (distance > 2000) {
+    return 'I will gladly take your thirty bucks.';
+  } else {
+    return 'This is not a valid distance';
+  }
+}
+function ternaryCheckCity(destination) {
+  return destination === 'NYC' ? 'Ok, sounds good.' : 'No go.';
+}
+function switchOnCharmFromTip(tip) {
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+      return 'Bye.';
+  }
+}
+function scuberGreetingForFeet(distance) {
+  if (distance <= 400) {
+    return 'This one is on me!';
+  } else if (distance > 2500) {
+    return 'No can do.';
+  } else if (distance > 2000) {
+    return 'I will gladly take your thirty bucks.';
+  } else {
+    return 'This is not a valid distance';
+  }
+}
+function scuberGreetingForFeet(distance) {
+  if (distance <= 400) {
+    return 'This one is on me!';
+  } else if (distance > 2500) {
+    return 'No can do.';
+  } else if (distance > 2000) {
+    return 'I will gladly take your thirty bucks.';
+  } else {
+    return 'This is not a valid distance';
+  }
+}
+
